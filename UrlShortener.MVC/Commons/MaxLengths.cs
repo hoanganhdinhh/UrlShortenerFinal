44109845a0c537e5e881f1b1557dd50d1635a87d
Dinh Anh <da9876b@gre.ac.uk>
2025-11-07 13:29:38 +0700

    Add project files.

---
+++ UrlShortener.MVC/Commons/MaxLengths.cs
@@ -0,0 +1,9 @@
+﻿namespace URLShortener.MVC.Commons
+{
+    public static class MaxLengths
+    {
+        public const int OriginalUrl = 4096;
+        public const int ShortCode = 255;
+        public const int QRCodeData = 8192;
+    }
+}
