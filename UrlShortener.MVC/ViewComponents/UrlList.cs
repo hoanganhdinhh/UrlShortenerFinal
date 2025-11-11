60843e038bbda8e883b9ff6001080c8cb9940588
LuuBao <luunguyentanbao@gmail.com>
2025-11-11 13:56:51 +0700

    adjust list view only seen by who created

--- UrlShortener.MVC/ViewComponents/UrlList.cs
+++ UrlShortener.MVC/ViewComponents/UrlList.cs
@@ -2,21 +2,33 @@
 using Microsoft.EntityFrameworkCore;
 using UrlShortener.MVC.Data;
 using UrlShortener.MVC.Models;
+using Microsoft.AspNetCore.Identity;
+using UrlShortener.MVC.Data.Entities.Identities;
 
 namespace UrlShortener.MVC.ViewComponents
 {
     public class UrlListViewComponent : ViewComponent
     {
         private readonly UrlShortenerDbContext _context;
-        public UrlListViewComponent(UrlShortenerDbContext context)
+        private readonly UserManager<UrlShortenerUser> _userManager;
+        
+        public UrlListViewComponent(UrlShortenerDbContext context, UserManager<UrlShortenerUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+        
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            // Get the current user's ID
+            var userId = _userManager.GetUserId(HttpContext.User);
+            
+            // Filter URLs to only show the current user's URLs
             var urlEntities = await _context.Urls
+                .Where(u => u.UserId == userId)
                 .OrderByDescending(u => u.CreatedAt)
                 .ToListAsync();
+    
             var viewModels = urlEntities.Select(u => new UrlVM
             {
                 Id = u.Id,
